200ea026c66669c622377241bbe639ebf22c8425
Arne Brasseur <arne@arnebrasseur.net>
2020-12-11 08:18:56 +0100

    Add day 11

---
+++ src/lambdaisland/aoc_2020/puzzle11.clj
@@ -0,0 +1,155 @@
+(ns lambdaisland.aoc-2020.puzzle11
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def demo-input "L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL")
+
+(defn parse-input [input]
+  (mapv #(mapv '{\. _ \L L} %) (str/split-lines input)))
+
+(def demo-layout (parse-input demo-input))
+(def real-layout (parse-input (slurp (io/resource "puzzle_input_11.txt"))))
+
+;; (def max-x (dec (count (first layout))))
+;; (def max-y (dec (count layout)))
+
+(defn neighbours [x y max-x max-y]
+  (for [x' (range (max 0 (dec x)) (inc (min max-x (inc x))))
+        y' (range (max 0 (dec y)) (inc (min max-y (inc y))))
+        :when (not (and (= x x') (= y y')))]
+    [x' y']))
+
+(defn pos-score
+  #_([layout x y]
+     (pos-score x y max-x max-y))
+  ([layout x y max-x max-y]
+   (reduce (fn [score coords]
+             (+ score (if (= 'O (get-in layout coords)) 1 0)))
+           0
+           (neighbours x y max-x max-y))))
+
+(defn generation
+  #_([layout]
+     (generation layout max-x max-y))
+  ([layout max-x max-y]
+   (mapv (fn [x]
+           (mapv (fn [y]
+                   (case (get-in layout [x y])
+                     _ '_
+                     L (if (= 0 (pos-score layout x y max-x max-y)) 'O 'L)
+                     O (if (<= 4 (pos-score layout x y max-x max-y)) 'L 'O)))
+                 (range (inc max-y))))
+         (range (inc max-x)))))
+
+(defn compute-max-x [layout]
+  (dec (count layout)))
+
+(defn compute-max-y [layout]
+  (dec (count (first layout))))
+
+(defn find-fix-point
+  ([layout]
+   (find-fix-point layout generation))
+  ([layout generation]
+   (let [max-x (compute-max-x layout)
+         max-y (compute-max-y layout)]
+     (loop [layout layout
+            layout' (generation layout max-x max-y)]
+       (if (= layout' layout)
+         layout
+         (recur layout' (generation layout' max-x max-y)))))))
+
+(comment
+  (pos-score (generation layout) 9 2)
+  (generation (generation layout)))
+
+(defn result1
+  ([layout]
+   (result1 layout generation))
+  ([layout generation]
+   (transduce (comp cat (map '{_ 0, L 0, O 1})) + (find-fix-point layout generation))))
+
+(comment
+  (result1 demo-layout)
+  (time (result1 real-layout)))
+;; => 2552
+
+(defn layout-size [l]
+  [(count l)
+   (count (first l))])
+
+(defn spit-layout [f l]
+  (spit f (str/join "\n" (map #(apply str (map '{_ \. L \L O \#} %)) l))))
+
+(comment
+  (spit-layout "/tmp/layout.txt" real-layout #_(generation real-layout
+                                                           (compute-max-x real-layout)
+                                                           (compute-max-y real-layout)))
+
+  (layout-size (generation real-layout
+                           (compute-max-x real-layout)
+                           (compute-max-y real-layout))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Part 2
+
+(def directions (for [x (range -1 2)
+                      y (range -1 2)
+                      :when (not= [x y] [0 0])]
+                  [x y]))
+
+(defn seat-in-direction [layout x y max-x max-y [dx dy]]
+  (loop [x (+ x dx)
+         y (+ y dy)]
+    (if (or (< x 0) (< y 0) (< max-x x) (< max-y y))
+      0
+      (let [seat (get-in layout [x y])]
+        (case seat
+          L 0
+          O 1
+          _ (recur (+ x dx)
+                   (+ y dy)))))))
+
+(defn compute-seat [layout x y max-x max-y]
+  (reduce
+   (fn [acc dir]
+     (let [acc (+ acc (seat-in-direction layout x y max-x max-y dir))]
+       (if (= acc 5)
+         (reduced 5)
+         acc)))
+   0
+   directions))
+
+(defn generation2
+  #_([layout]
+     (generation layout max-x max-y))
+  ([layout max-x max-y]
+   (mapv (fn [x]
+           (mapv (fn [y]
+                   (let [sym (get-in layout [x y])]
+                     (if (= '_ sym)
+                       '_
+                       (case (compute-seat layout x y max-x max-y)
+                         5 'L
+                         0 'O
+                         sym))))
+                 (range (inc max-y))))
+         (range (inc max-x)))))
+
+(comment
+  (time (result1 real-layout generation2))
+  ;; => 2197
+  (generation2 demo-layout 9 9)
+
+  (compu)
+
+  )
